8a986f2d157de11f06eb5c2e7058e1596e2b9061
Damian <josepwnz@gmail.com>
2019-10-14 23:19:00 +0200

    Funciones de las nuevas funcionalidades implementadas

--- wp-content/plugins/simple-forum/class.simple-forum.php
+++ wp-content/plugins/simple-forum/class.simple-forum.php
@@ -3,120 +3,122 @@
 // Front-end
 class SimpleForum {
     
-    // Acciones a llevar a cabo cuando el plugin es activado
-    public function plugin_activation() {
-        global $wpdb;
-        
-        $queryUsers = "CREATE TABLE IF NOT EXISTS SPF_USERS(
-            id INT AUTO_INCREMENT NOT NULL,
-            username VARCHAR(50) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL,
-            email VARCHAR(100) NOT NULL UNIQUE,
-            PRIMARY KEY(id)
-        )";
-            
-        $queryCategories = "CREATE TABLE IF NOT EXISTS SPF_CATEGORIES(
-            id INT AUTO_INCREMENT NOT NULL,
-            name VARCHAR(50) NOT NULL UNIQUE,
-            description VARCHAR(255) NOT NULL,
-            PRIMARY KEY(id)
-        )";
-                
-                
-        $queryTopics = "CREATE TABLE IF NOT EXISTS SPF_TOPICS (
-            id INT AUTO_INCREMENT NOT NULL,
-            cat_id INT NOT NULL,
-            author_id INT NOT NULL,
-            title VARCHAR(100) NOT NULL,
-            created_at TIMESTAMP NOT NULL,
-            PRIMARY KEY(id),
-            FOREIGN KEY(cat_id) REFERENCES SPF_CATEGORIES(id),
-            FOREIGN KEY(author_id) REFERENCES SPF_USERS(id)
-        )";
-                    
-        $queryPosts = "CREATE TABLE IF NOT EXISTS SPF_POSTS (
-            id INT AUTO_INCREMENT NOT NULL,
-            topic_id INT NOT NULL,
-            author_id INT NOT NULL,
-            post_content TEXT NOT NULL,
-            posted_at TIMESTAMP NOT NULL,
-            PRIMARY KEY(id),
-            FOREIGN KEY(topic_id) REFERENCES SPF_TOPICS(id),
-            FOREIGN KEY(author_id) REFERENCES SPF_USERS(id)
-        )";
-                        
-        $querySettings = "CREATE TABLE IF NOT EXISTS SPF_SETTINGS(
-            spf_key VARCHAR(50) NOT NULL UNIQUE,
-            spf_value TEXT,
-            PRIMARY KEY(spf_key)
-        )";
-                            
-        $wpdb->query($queryUsers);
-        $wpdb->query($queryCategories);
-        $wpdb->query($queryTopics);
-        $wpdb->query($queryPosts);
-        $wpdb->query($querySettings);
-                            
-        //add_option( 'fmk', '' );
-    }
-
     // Forma segura de obtener valores de $_GET
     public function get_query_var( $var, $default = '' ) {
         global $wp_query;
         return $wp_query->get( $var, $default );
     }
-                        
-    // Acciones a llevar a cabo cuando el plugin es desactivado (deshacer todo lo hecho en activacion)
-    public function plugin_deactivation() {
-        global $wpdb;
-        $wpdb->query("DROP TABLE IF EXISTS SPF_SETTINGS");
-        $wpdb->query("DROP TABLE IF EXISTS SPF_USERS");
-        $wpdb->query("DROP TABLE IF EXISTS SPF_POSTS");
-        $wpdb->query("DROP TABLE IF EXISTS SPF_CATEGORIES");
-        $wpdb->query("DROP TABLE IF EXISTS SPF_TOPICS");
-        //delete_option( 'fmk' );
+ 
+    public function get_current_url() {
+        global $wp;
+        return add_query_arg( $wp->query_vars, home_url( $wp->request ));
+
     }
                         
-    // Desactivar y borrar plugin
-    public function plugin_uninstall() {
-    }
-                        
-    public function spf_have_categories() {
+    public function spf_get_forums() {
         global $wpdb;
         return $wpdb->get_results("SELECT id, name, description FROM SPF_CATEGORIES", ARRAY_A);
     }
                         
-    public function spf_have_topics($cat_id = -1) {
+    public function spf_get_topics($cat_id = 1) {
         global $wpdb;
         return $wpdb->get_results("SELECT SPF_TOPICS.id, SPF_TOPICS.title, SPF_USERS.username AS author FROM SPF_TOPICS INNER JOIN SPF_USERS ON SPF_TOPICS.author_id = SPF_USERS.id WHERE cat_id = '{$cat_id}'", ARRAY_A);
     }
                         
-    public function spf_show_topic($topic_id = -1) {
+    public function spf_get_topic($topic_id = 1) {
         global $wpdb;
         return $wpdb->get_results("SELECT SPF_POSTS.post_content, SPF_POSTS.posted_at, SPF_USERS.username FROM SPF_POSTS INNER JOIN SPF_USERS ON SPF_POSTS.author_id = SPF_USERS.id WHERE topic_id = '{$topic_id}'", ARRAY_A);
     }
-                        
-    // Shortcode para mostrar el foro en una pagina
-    public function show_forum($atts) {
-        include_once(PLUGIN_DIR . 'include/forum.php');
+
+    // Agrega un nuevo post a un topic ya iniciado
+    public function spf_create_post($topic_id, $author_id, $content) {
+        global $wpdb;
+        return $wpdb->insert('SPF_POSTS',
+            array(
+                'topic_id' => $topic_id, 
+                'author_id' => $author_id,
+                'post_content' => $content
+            ),
+            array('%d', '%d','%s')
+        );
+    }
+
+    // Agrega un nuevo topic a una categoria
+    public function spf_create_topic($cat_id, $author_id, $title, $content) {
+        global $wpdb;
+        $wpdb->insert('SPF_TOPICS',
+            array(
+                'cat_id' => $cat_id, 
+                'author_id' => $author_id,
+                'title' => $title
+            ),
+            array('%d', '%d','%s')
+        );
+        $topic_id = $wpdb->insert_id;
+        return $this->spf_create_post($topic_id, $author_id, $content);
+    }
+
+    public function spf_create_user($username, $password, $email) {
+        global $wpdb;
+        return $wpdb->insert('SPF_USERS',
+            array(
+                'username' => $username, 
+                'password' => $password,
+                'email' => $email
+            ),
+            array('%s', '%s','%s')
+        );
+    }
+
+    public function spf_login_user($username, $password) {
+
     }
                         
+    // Shortcode para mostrar el foro en una pagina
+    public function spf_show_forums($atts) {
+        include_once(PLUGIN_DIR . 'include/forums.php');
+    }
+
+    public function spf_show_topics($atts) {
+        include_once(PLUGIN_DIR . 'include/topics.php');
+    }
+
+    public function spf_show_posts($atts) {
+        include_once(PLUGIN_DIR . 'include/posts.php');
+    }
+
+    public function spf_show_login($atts) {
+        include_once(PLUGIN_DIR . 'include/login.php');
+    }
+
+    public function spf_show_register($atts) {
+        include_once(PLUGIN_DIR . 'include/register.php');
+    }
+
+    // Comprueba si el mod esta habilitado, si no lo esta el plugin no funcionara
+    public function check_rewrite_enabled() {
+        // return
+    }
+
+    public function choose_font() {
+        //enqueue font
+    }
+        
     public function init_hooks() {
-        add_shortcode('forum', array($this, 'show_forum'));
+        add_shortcode('spf_show_forums', array($this, 'spf_show_forums'));
+        add_shortcode('spf_show_topics', array($this, 'spf_show_topics'));
+        add_shortcode('spf_show_posts', array($this, 'spf_show_posts'));
+        add_shortcode('spf_show_login', array($this, 'spf_show_login'));
+        add_shortcode('spf_show_register', array($this, 'spf_show_register'));
     }
                         
     public function init() {
-        //wp_register_script('prefix_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js');
         wp_register_script('prefix_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js');
-        wp_enqueue_script('prefix_bootstrap');
-          
+        wp_enqueue_script('prefix_bootstrap');        
         wp_register_style('prefix_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-        //wp_register_style('prefix_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css');
         wp_enqueue_style('prefix_bootstrap');
-                            
         wp_register_style('spf_style', plugins_url('simple-forum/include/css/spf.css'));
         //wp_enqueue_style('spf_style');
-                            
         $this->init_hooks();
     }
 }

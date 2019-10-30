3fe990175d57ee510c4ef087bc74b66d85369480
damianS7 <josepwnz@gmail.com>
2019-10-30 16:26:07 +0100

    cambio de tab a spaces

--- wp-content/plugins/simple-forum/public/views/forums.php
+++ wp-content/plugins/simple-forum/public/views/forums.php
@@ -1,18 +1,18 @@
 <?php include_once('userbar.php'); ?>
 <?php include_once('breadcrumb.php'); ?>
 <?php if (is_array($data['forums'])): ?>
-	<div class="card text-white bg-primary mb-3">
-		<div class="card-header">Forums</div>
-		<?php foreach ($data['forums'] as $forum): // Listado de categorias?>
-			<div class="card-body">
-				<h4 class="card-title">
-					<a href="<?php echo get_permalink() . "topics/". $forum['id']; ?>" class="card-link">
-						<?php echo $forum['name']; ?>
-					</a>
-				</h4>
-				<p class="card-text"><?php echo $forum['description']; ?></p>
-			</div>
-		<?php endforeach; ?>
-	</div>
+    <div class="card text-white bg-primary mb-3">
+        <div class="card-header">Forums</div>
+        <?php foreach ($data['forums'] as $forum): // Listado de categorias?>
+            <div class="card-body">
+                <h4 class="card-title">
+                    <a href="<?php echo get_permalink() . "topics/". $forum['id']; ?>" class="card-link">
+                        <?php echo $forum['name']; ?>
+                    </a>
+                </h4>
+                <p class="card-text"><?php echo $forum['description']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 <?php endif; ?>
 

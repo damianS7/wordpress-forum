045a7baabb68ef563b6e0c9a5d1747a09458e77e
damianS7 <josepwnz@gmail.com>
2019-10-30 16:01:24 +0100

    Metodo corregido

--- wp-content/plugins/simple-forum/public/views/userbar.php
+++ wp-content/plugins/simple-forum/public/views/userbar.php
@@ -1,4 +1,4 @@
-<?php if (SimpleForumAuth::is_auth()): ?>
+<?php if (SPF_Auth::is_auth()): ?>
 <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,10 +18,12 @@
         </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
     </nav>
 </div>
+<?php else: ?>
+    
 <?php endif; ?>

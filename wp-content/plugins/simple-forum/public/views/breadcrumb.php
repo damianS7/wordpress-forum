314040bfd9391a1f1baa3b056c2d6fe8ea0d8d53
damianS7 <josepwnz@gmail.com>
2019-10-30 16:02:38 +0100

    Nueva forma de detectar la pagina

--- wp-content/plugins/simple-forum/public/views/breadcrumb.php
+++ wp-content/plugins/simple-forum/public/views/breadcrumb.php
@@ -1,13 +1,13 @@
 <div class="mt-3">
     <ol class="breadcrumb">
-        <?php if (strpos(get_permalink(), 'spf-show-topics') !== false): ?>
-            <li class="breadcrumb-item"><a href="<?php echo home_url() . '/spf-show-forums'; ?>">SPF Forums</a></li>
+        <?php if (strpos(get_query_var('spf_view'), 'topics') !== false): ?>
+            <li class="breadcrumb-item"><a href="<?php echo get_permalink() . 'forums'; ?>">SPF Forums</a></li>
             <li class="breadcrumb-item"><?php echo $data['forum']->name; ?></li>
-        <?php elseif (strpos(get_permalink(), 'spf-show-posts') !== false): ?>
-            <li class="breadcrumb-item"><a href="<?php echo home_url() . '/spf-show-forums'; ?>">SPF Forums</a></li>
-            <li class="breadcrumb-item"><a href="<?php echo home_url() . '/spf-show-topics/' . $data['topic']->cat_id; ?>"><?php echo $data['topic']->subforum; ?></a></li>
+        <?php elseif (strpos(get_query_var('spf_view'), 'posts') !== false): ?>
+            <li class="breadcrumb-item"><a href="<?php echo get_permalink() . 'forums'; ?>">SPF Forums</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo get_permalink() . 'topics/' . $data['topic']->forum_id; ?>"><?php echo $data['topic']->subforum; ?></a></li>
             <li class="breadcrumb-item"><?php echo substr($data['topic']->title, 0, 5); ?></li>
-        <?php elseif (strpos(get_permalink(), 'spf-show-forums') !== false): ?>
+        <?php elseif (strpos(get_query_var('spf_view'), 'forums') !== false): ?>
             <li class="breadcrumb-item">SPF Forums</li>
         <?php endif; ?>
     </ol>

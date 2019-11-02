5c35ced15ff3d07b35f9c8e00fd6487cbbf45c50
damianS7 <josepwnz@gmail.com>
2019-11-02 21:52:41 +0100

    cambio de nombre para el fichero

---
+++ wp-content/plugins/simple-forum/public/views/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo SimpleForum::view_url('forums'); ?>">Forum</a>        
+            </li> 
+            <?php if (SPF_AccountController::is_auth()): ?>
+                <li class="nav-item">
+                    <p> Welcome 
+                        <strong><?php echo $_SESSION['account']->username;?></strong>
+                    </p>
+                </li>  
+            <?php endif; ?>
+            <li class="nav-item">
+                <?php if (SPF_AccountController::is_auth()): ?>
+                    <a class="nav-link" href="<?php echo SimpleForum::view_url('profile'); ?>">Profile</a>
+                <?php else: ?>
+                    <a class="nav-link" href="<?php echo SimpleForum::view_url('login'); ?>">Login</a>
+                <?php endif; ?>
+            </li>
+        <li class="nav-item">
+            <?php if (SPF_AccountController::is_auth()): ?>
+                <a class="nav-link" href="<?php echo SimpleForum::view_url('logout'); ?>">Logout</a>
+            <?php else: ?>
+                <a class="nav-link" href="<?php echo SimpleForum::view_url('register') ?>">Register</a>
+            <?php endif; ?>
+        </li>
+    </ul>
+    <form method="POST" action="<?php echo SimpleForum::view_url('search'); ?>" class="form-inline">
+        <input class="form-control input-sm" name="query" type="text" placeholder="Search">
+    </form>
+    </div>
+</nav> 

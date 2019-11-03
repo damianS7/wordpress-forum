1ddbeaa1f82edea94d04f6073af63105f1d2c770
damianS7 <josepwnz@gmail.com>
2019-11-03 22:47:30 +0100

    implementacion de patron MVC para el backend

--- wp-content/plugins/simple-forum/admin/controllers/class.admin-forum-controller.php
+++ wp-content/plugins/simple-forum/admin/controllers/class.admin-forum-controller.php
@@ -1,6 +1,27 @@
 <?php
 
 class SPF_AdminForumController {
-    public static function add_forum() {
+
+    // Controlador de la vista "forums"
+    public function view_forums() {
+        // Creacion de forum
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['create_forum'])) {
+            $name = sanitize_text_field($_POST['name']);
+            $description = sanitize_text_field($_POST['description']);
+            if (SPF_AdminForum::create_forum($name, $description)) {
+                $data['success_message'] = 'The forum has been created.';
+            }
+        }
+
+        // Borrado de foro
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_forum'])) {
+            $forum_id = sanitize_text_field($_POST['forum_id']);
+            if (SPF_AdminForum::delete_forum($forum_id) !== null) {
+                $data['success_message'] = 'The forum has been deleted.';
+            }
+        }
+
+        $data['forums'] = SPF_AdminForum::get_forums();
+        SimpleForumAdmin::view('forums.php', $data);
     }
 }
--- wp-content/plugins/simple-forum/admin/simple-forum-admin.php
+++ wp-content/plugins/simple-forum/admin/simple-forum-admin.php
@@ -3,48 +3,18 @@ require_once(PLUGIN_DIR . 'admin/controllers/class.admin-forum-controller.php');
 require_once(PLUGIN_DIR . 'admin/models/class.admin-forum-model.php');
 
 class SimpleForumAdmin {
+    private $forum_controller;
+    
     public function __construct() {
+        $this->forum_controller = new SPF_AdminForumController();
     }
 
     public function plugin_menu() {
         add_menu_page('SimpleForum', 'Simple Forum', 'manage_options', 'simpleforum-menu', array($this, 'view_settings'));
-        add_submenu_page('simpleforum-menu', 'SimpleForum', 'Moderation', 'manage_options', 'simpleforum-moderation', array($this, 'view_moderation' ));
-        add_submenu_page('simpleforum-menu', 'SimpleForum', 'Users', 'manage_options', 'simpleforum-users', array($this, 'view_users' ));
-        add_submenu_page('simpleforum-menu', 'SimpleForum', 'Forums', 'manage_options', 'simpleforum-forums', array($this, 'view_forums' ));
-    }
-
-    public function view_settings() {
-        include_once(PLUGIN_DIR . 'admin/views/settings.php');
-    }
-
-    public function view_moderation() {
-        include_once(PLUGIN_DIR . 'admin/views/moderation.php');
-    }
-
-    public function view_users() {
-        include_once(PLUGIN_DIR . 'admin/views/users.php');
-    }
-
-    public function view_forums() {
-        // Creacion de forum
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['create_forum'])) {
-            $name = sanitize_text_field($_POST['name']);
-            $description = sanitize_text_field($_POST['description']);
-            if (SPF_AdminForum::create_forum($name, $description)) {
-                $data['success_message'] = 'The forum has been created.';
-            }
-        }
-
-        // Borrado de foro
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_forum'])) {
-            $forum_id = sanitize_text_field($_POST['forum_id']);
-            if (SPF_AdminForum::delete_forum($forum_id) !== null) {
-                $data['success_message'] = 'The forum has been deleted.';
-            }
-        }
-
-        $data['forums'] = SPF_AdminForum::get_forums();
-        SimpleForumAdmin::view('forums.php', $data);
+        add_submenu_page('simpleforum-menu', 'SimpleForum', 'Forums', 'manage_options', 'simpleforum-forums', array($this->forum_controller, 'view_forums'));
+        //add_submenu_page('simpleforum-menu', 'SimpleForum', 'Moderation', 'manage_options', 'simpleforum-moderation', array($this, 'controller' ));
+        //add_submenu_page('simpleforum-menu', 'SimpleForum', 'Users', 'manage_options', 'simpleforum-users', array($this, 'view_users' ));
+        //add_submenu_page('simpleforum-menu', 'SimpleForum', 'Forums', 'manage_options', 'simpleforum-forums', array(new SPF_AdminForumController(), 'view_forums'));
     }
 
     public static function view($view, $data) {

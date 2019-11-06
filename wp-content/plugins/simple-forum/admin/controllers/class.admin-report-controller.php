d7051975eca3d26003d501804091fe82a7c3464f
damianS7 <josepwnz@gmail.com>
2019-11-06 17:17:30 +0100

    reorganizacion de controlador

--- wp-content/plugins/simple-forum/admin/controllers/class.admin-report-controller.php
+++ wp-content/plugins/simple-forum/admin/controllers/class.admin-report-controller.php
@@ -2,9 +2,30 @@
 
 class SPF_Admin_ReportController {
     public function view_reports() {
-        $data = array();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            return $this->handle_forms();
+        }
 
+        return $this->render();
+    }
+
+    public function handle_forms() {
+        $report_id = sanitize_text_field($_POST['report_id']);
+        // BAN
+        if (isset($_POST['ban_account'])) {
+            $account_id = sanitize_text_field($_POST['account_id']);
+            if (SPF_Admin_Account::ban_account($account_id)) {
+                if (SPF_Admin_Report::delete_report($report_id)) {
+                    $data['success_message'] = 'The account has been banned.';
+                }
+            }
+
+            return $this->render($data);
+        }
+    }
+
+    public function render($data = array()) {
         $data['reports'] = SPF_Admin_Report::get_reports();
-        SimpleForumAdmin::view('reports.php', $data);
+        return SimpleForumAdmin::view('reports.php', $data);
     }
 }

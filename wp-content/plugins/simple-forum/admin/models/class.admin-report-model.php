bfae6d1861da0afcd7730189be406afaa77381d8
damianS7 <josepwnz@gmail.com>
2019-11-06 17:16:49 +0100

    Nuevos campos para la consulta y metodo para borrar reports.

--- wp-content/plugins/simple-forum/admin/models/class.admin-report-model.php
+++ wp-content/plugins/simple-forum/admin/models/class.admin-report-model.php
@@ -5,14 +5,31 @@ class SPF_Admin_Report {
         global $wpdb;
         $query = "SELECT
             SPF_REPORTS.id,
+            SPF_REPORTS.post_id,
             SPF_REPORTS.report AS reason, 
+            SPF_REPORTS.reporter_id, 
             SPF_ACCOUNTS.username AS post_owner, 
-            SPF_POSTS.post_content
+            SPF_POSTS.post_content,
+            SPF_POSTS.topic_id
             FROM SPF_REPORTS 
             INNER JOIN SPF_ACCOUNTS 
             ON SPF_REPORTS.reporter_id = SPF_ACCOUNTS.id
             INNER JOIN SPF_POSTS
             ON SPF_REPORTS.post_id = SPF_POSTS.id";
         return $wpdb->get_results($query);
+    }
+
+    public static function delete_report($report_id) {
+        global $wpdb;
+        $where = array(
+            'id' => $report_id
+        );
+
+        // Si no se borra nada
+        if (!$wpdb->delete('SPF_REPORTS', $where)) {
+            return false;
+        }
+
+        return true;
     }
 }
